69f372a8df5bbba08a9db9ec3baa4698b25ec526
MCristina0 <168736210+MCristina0@users.noreply.github.com>
2024-10-24 11:44:00 -0500

    con crudd

---
+++ bdJavaMariaCristinaMorenoSanchez/bd.sql
@@ -0,0 +1,11 @@
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS taller_jdbc;
+USE taller_jdbc;
+
+-- Crear la tabla empleados
+CREATE TABLE empleados (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    cargo VARCHAR(50) NOT NULL,
+    salario double not null
+    );
